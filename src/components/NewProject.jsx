6047c0acbe7aaed8cd44129d4b83bf8dcfebb6df
Serhii Demchenko <svdemchenko@gmail.com>
2024-01-27 16:36:10 +0200

    Update NewProject.jsx
    add action style

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -51,7 +51,7 @@ const NewProject = ({ onAdd, onCancel }) => {
 					<li>
 						<button
 							onClick={onCancel}
-							className="text-stone-800 hover:text-stone-950"
+							className="text-stone-600 hover:text-stone-950 hover:scale-105  "
 						>
 							Cancel
 						</button>
@@ -59,7 +59,7 @@ const NewProject = ({ onAdd, onCancel }) => {
 					<li>
 						<button
 							onClick={handleSave}
-							className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+							className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 hover:scale-105"
 						>
 							Save
 						</button>
